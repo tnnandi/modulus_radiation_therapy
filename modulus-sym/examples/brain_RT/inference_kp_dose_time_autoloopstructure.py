480fc9c81796c74459d1d462d4e3ef0efbdfce93
tnnandi <tnnandi@gmail.com>
2025-01-29 00:40:03 -0600

    inference script for writing out files compatible with paraview frame progress

--- modulus-sym/examples/brain_RT/inference_kp_dose_time_autoloopstructure.py
+++ modulus-sym/examples/brain_RT/inference_kp_dose_time_autoloopstructure.py
@@ -1,12 +1,14 @@
 import os
 import torch
 import numpy as np
+from sympy import Symbol, sqrt, Max, Eq
 import matplotlib.pyplot as plt
 from modulus.sym.utils.io.vtk import var_to_polyvtk
 from modulus.sym.hydra import to_absolute_path, instantiate_arch, ModulusConfig
 from modulus.sym.key import Key
 from modulus.sym.geometry.tessellation import Tessellation
 import modulus.sym
+from moving_time_window import MovingTimeWindowArch
 
 # execute within a modulus container on polaris
 # cd /mnt/Modulus_24p04/modulus-sym/examples/brain_RT
@@ -18,17 +20,17 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # function to load trained model and carry out inference at specified points
 def run_inference(cfg,
-                  vary_time=False,
-                  vary_dose=False,
-                  vary_kp=False,
+                  # vary_time=False,
+                  # vary_dose=False,
+                  # vary_kp=False,
                   inference_times=None,
                   inference_doses=None,
                   inference_kps=None,
                   checkpoint_dir=None,
                   output_dir=None):
 
-    if sum([vary_time, vary_dose, vary_kp]) != 1:
-        raise ValueError("Please specify exactly one parameter to vary: time, dose, or kp.")
+    # if sum([vary_time, vary_dose, vary_kp]) != 1:
+    #     raise ValueError("Please specify exactly one parameter to vary: time, dose, or kp.")
 
     if inference_times is None or inference_doses is None or inference_kps is None:
         raise ValueError("Please supply inference values for time, dose, and kp.")
@@ -48,54 +50,83 @@ def run_inference(cfg,
     interior_mesh.scale(scale)
 
     # override defaults
-    cfg.arch.fully_connected.layer_size = 128
+    cfg.arch.fully_connected.layer_size = 64 #128
     cfg.arch.fully_connected.nr_layers = 4
 
     tumor_net = instantiate_arch(
         input_keys=[Key("x"), Key("y"), Key("z"), Key("t"), Key("kp"), Key("dose")],
+        # add "kp" and "t" as additional input (parameterized)
         output_keys=[Key("N")],
         cfg=cfg.arch.fully_connected,
     )
 
-    tumor_net.to(device)
-    tumor_net.eval()
+    # moving window approach for time evolution over larger durations
+    # parameterized time
+    time_window_size = 1.0
+    alpha_value = 0.035
+    alpha_by_beta_value = 10
+    t_treatment = [1, 2, 3, 4, 5, 8, 9, 10, 11, 12, 15, 16, 17, 18, 19] # using zero-based indexing
 
-    # load checkpoint
-    checkpoint_network = torch.load(os.path.join(checkpoint_dir, "flow_network.0.pth"), map_location=device)
-    tumor_net.load_state_dict(checkpoint_network)
-    print(f"Model loaded with {sum(p.numel() for p in tumor_net.parameters() if p.requires_grad)} trainable parameters")
+    # Iterate over the checkpoint directories for each time window
+    if len(inference_times) == 1:
+        # for a fixed time window
+        fixed_time_index = int(inference_times[0])  # Ensure time index is an integer (and not a list)
+        time_window_dirs = [
+            d for d in os.listdir(checkpoint_dir)
+            if d.startswith(f"time_window_{fixed_time_index:04d}")
+        ]
+    # elif len(inference_times) > 1:
+    #     # For a specific list of time windows
+    #     chosen_time_indices = [int(t) for t in inference_times]  # Ensure all indices are integers
+    #     time_window_dirs = [
+    #         d for d in os.listdir(checkpoint_dir)
+    #         if any(d.startswith(f"time_window_{idx:04d}") for idx in chosen_time_indices)
+    #     ]
+    else:
+        # For all time windows
+        time_window_dirs = [
+            d for d in os.listdir(checkpoint_dir)
+            if d.startswith("time_window_")
+        ]
 
-    # iterate over time and kp values for inference
-    interior_points = interior_mesh.sample_interior(1000000)
-    invar = {k: torch.tensor(v, dtype=torch.float32).to(device) for k, v in interior_points.items()}
+    for time_window_dir in time_window_dirs:
+        print("Loading ", time_window_dir)
+        # Extract the actual time index from the directory name
+        time_idx = int(time_window_dir.split("_")[-1])  # Get the time index from the directory name
+        checkpoint_path = os.path.join(checkpoint_dir, time_window_dir, "time_window_network.0.pth")
+        if not os.path.exists(checkpoint_path):
+            print(f"Checkpoint not found: {checkpoint_path}")
+            continue
 
-    indices = {
-        "time": (inference_times, 't'),
-        "dose": (inference_doses, 'dose'),
-        "kp": (inference_kps, 'kp')
-    }
+        time_window_net = MovingTimeWindowArch(tumor_net,
+                                               time_window_size,
+                                               alpha=alpha_value,
+                                               alpha_by_beta=alpha_by_beta_value,
+                                               t_treatment=t_treatment)
+        time_window_net.to(device)
+        time_window_net.load_state_dict(torch.load(checkpoint_path, map_location=device))
+        time_window_net.eval()
+        print(f"Loaded model for time window {time_idx} from {checkpoint_path}")
 
-    if vary_time:
-        outer_loop, middle_loop, inner_loop = indices["dose"], indices["kp"], indices["time"]
-    elif vary_dose:
-        outer_loop, middle_loop, inner_loop = indices["kp"], indices["time"], indices["dose"]
-    elif vary_kp:
-        outer_loop, middle_loop, inner_loop = indices["dose"], indices["time"], indices["kp"]
+        # Sample interior points for inference
+        interior_points = interior_mesh.sample_interior(1000000)
+        invar = {k: torch.tensor(v, dtype=torch.float32).to(device) for k, v in interior_points.items()}
 
-    # run inference
-    for outer_value in outer_loop[0]:
-        for middle_value in middle_loop[0]:
-            for inner_value in inner_loop[0]:
-                invar[outer_loop[1]] = torch.full_like(invar['x'], outer_value)
-                invar[middle_loop[1]] = torch.full_like(invar['x'], middle_value)
-                invar[inner_loop[1]] = torch.full_like(invar['x'], inner_value)
+        # Set the fixed time value for the current time window
+        fixed_time = time_idx * time_window_size
+        invar["t"] = torch.full_like(invar["x"], fixed_time)
 
-                print(
-                    f"Inference for {outer_loop[1]}: {outer_value}, {middle_loop[1]}: {middle_value}, {inner_loop[1]}: {inner_value}")
+        # Run inference for varying dose and kp
+        for kp in inference_kps:
+            invar["kp"] = torch.full_like(invar["x"], kp)
+            for dose in inference_doses:
+                invar["dose"] = torch.full_like(invar["x"], dose)
 
-                # inference
+                print(f"Inference for time: {fixed_time}, kp: {kp}, dose: {dose}")
+
+                # Perform inference
                 with torch.no_grad():
-                    outvar = tumor_net(invar)
+                    outvar = time_window_net(invar)
                     save_var = {
                         "N": outvar["N"].detach().cpu().numpy(),
                         "x": invar["x"].detach().cpu().numpy(),
@@ -103,54 +134,76 @@ def run_inference(cfg,
                         "z": invar["z"].detach().cpu().numpy(),
                     }
 
-                    # save outputs in VTK format
-                    output_filename = f"field_tessellated_interior_{outer_loop[1]}_{outer_value}_{middle_loop[1]}_{middle_value}_{inner_loop[1]}_{inner_value}.vtk"
+                    # Save outputs in VTK format
+                    # output_filename = f"field_tessellated_interior_time_{time_idx}_kp_{kp}_dose_{dose}.vtk"
+
+                    # Dynamically construct the filename with varying quantity at the end
+                    if len(inference_times) > 1:
+                        # Time is varying
+                        output_filename = f"field_tessellated_interior_kp_{kp}_dose_{dose}_time_{time_idx}.vtk"
+                    elif len(inference_doses) > 1:
+                        # Dose is varying
+                        output_filename = f"field_tessellated_interior_kp_{kp}_time_{time_idx}_dose_{dose}.vtk"
+                    elif len(inference_kps) > 1:
+                        # kp is varying
+                        output_filename = f"field_tessellated_interior_dose_{dose}_time_{time_idx}_kp_{kp}.vtk"
+                    else:
+                        # Default to time varying
+                        output_filename = f"field_tessellated_interior_time_{time_idx}_kp_{kp}_dose_{dose}.vtk"
+
                     output_filepath = os.path.join(output_dir, output_filename)
-                    print(f"Saving to: {output_filename}")
+                    print(f"Saving to: {output_filepath}")
                     var_to_polyvtk(save_var, output_filepath)
 
 @modulus.sym.main(config_path="conf", config_name="config")
 def main(cfg: ModulusConfig) -> None:
-    # edit output_dir based on the parameters chosen
+    # Directory for checkpoints and output
     checkpoint_dir = to_absolute_path(
-        "./outputs/brain_param_kp_dose_time/brain_param_kp_dose_time.lambda_1_100_50.heaviside_source_plus/")
-    # output_dir = to_absolute_path("./postprocess_output.vary_t_kp_0p1_dose_8")
-    output_dir = to_absolute_path("./postprocess_output.vary_dose_t_0_kp_0p2")
-    # output_dir = to_absolute_path("./postprocess_output.vary_kp_t_10_dose_2")
-    os.makedirs(output_dir, exist_ok=True)
+        "./outputs/brain_param_kp_dose_time.windowing/brain_param_kp_dose_time.lambda_1_100_100_1000.layer_size_64_test/"
+    )
+    # output_dir = to_absolute_path("./postprocess_output.vary_kp_dose_fixed_time")
+    # output_dir = to_absolute_path("./postprocess_output.vary_dose_time_3_kp_0p05")
+    # output_dir = to_absolute_path("./postprocess_output.vary_time_dose_1_kp_0p05")
 
-    # # time and kp values for inference
-    # # inference_times = [0.0, 5.0, 10.0, 15.0]
-    # # inference_times = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
-    #
-    # inference_times = [i for i in range(20)]
-    # inference_kps = [0.02, 0.05, 0.1, 0.2, 0.3, 0.5]
-    # inference_doses = [i for i in range(11)]
-    #
-    # run_inference(cfg, checkpoint_dir, output_dir, inference_times, inference_kps, inference_doses)
+    # Inference parameters
+    # inference_times = [3, 6] #
+    inference_times = [i for i in range(20)]  # Time indices (fixed by outer loop)
+    inference_doses = [4] #[i for i in range(9)]  # Varying dose
+    inference_kps = [0.05] #[0.05, 0.1, 0.2]  # Varying kp
 
-    # inference_times = [i for i in range(20)]
-    # inference_kps = [0.02, 0.03, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7]
-    inference_doses = [i for i in range(11)]
+    # Determine the varying quantity and construct the directory name dynamically
+    if len(inference_times) > 1:
+        # Time is varying
+        varying_param = "vary_time"
+        fixed_params = f"dose_{inference_doses[0]}_kp_{inference_kps[0]}"
+    elif len(inference_doses) > 1:
+        # Dose is varying
+        varying_param = "vary_dose"
+        fixed_params = f"time_{inference_times[0]}_kp_{inference_kps[0]}"
+    elif len(inference_kps) > 1:
+        # kp is varying
+        varying_param = "vary_kp"
+        fixed_params = f"time_{inference_times[0]}_dose_{inference_doses[0]}"
+    else:
+        # Default to time varying if all are fixed (unlikely)
+        varying_param = "vary_time"
+        fixed_params = f"dose_{inference_doses[0]}_kp_{inference_kps[0]}"
 
-    # Example usage with external values
-    inference_times = [0]
-    # inference_doses = [8]
-    inference_kps = [0.2]
+    from datetime import datetime
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")  # Format: YYYYMMDD_HHMMSS
+    output_dir = to_absolute_path(f"./postprocess_output.{varying_param}_{fixed_params}_{timestamp}")
 
-    # choose True for either vary_time/dose/k_p
+    print(f"Output directory: {output_dir}")
+
+    # Run inference
     run_inference(
         cfg,
-        vary_time=False,
-        vary_dose=True,
-        vary_kp=False,
         inference_times=inference_times,
         inference_doses=inference_doses,
         inference_kps=inference_kps,
         checkpoint_dir=checkpoint_dir,
         output_dir=output_dir
     )
-
 
 if __name__ == "__main__":
     main()
